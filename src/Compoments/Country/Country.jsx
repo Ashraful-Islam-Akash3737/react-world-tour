71f0cfa8cdb8aee515f7f973523728e7ac098117
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-03-11 10:37:12 +0600

    add toggle button to show the visited and non-visited country.

--- src/Compoments/Country/Country.jsx
+++ src/Compoments/Country/Country.jsx
@@ -1,10 +1,23 @@
 // import React from 'react';
+import { useState } from 'react';
 import './Country.css'
 
 const Country = ({country}) => {
     console.log(country)
-    const {name, flags, population, area, capital, continents, currencies, independent, languages
-    } = country;
+    const {name, flags, population, area, capital, continents, independent, languages , cca3} = country;
+
+    const [visited, setVisited] = useState(false);
+    const handleVisitedCountry = (e) => {
+        setVisited(!visited);
+        if (visited) {
+            const visitedCountry = e.target;
+            visitedCountry.className = "visited-country";
+        } else {
+            const goingCountry = e.target;
+            goingCountry.className = "going-country";
+        }
+    }
+
     return (
         <div className="country">
             <h3>Name: {name?.common} </h3>
@@ -13,10 +26,14 @@ const Country = ({country}) => {
             <p>Population: {population} </p>
             <p>Area: {area} </p>
             <p>Continents: {continents} </p>
-            <p>Currencies: {currencies?.HKD?.name} </p>
             <p>Independent: {independent ? 'Yes' : 'No'} </p>
-            <p>Languages: {languages?.eng} {languages?.zho}</p>
-
+            <p><small>Code: {cca3} </small></p>
+            <p>Languages: {languages?.eng ? languages?.eng : "Unknown"}</p>
+            <br />
+            <button className={visited ? "visited-country" : "going-country"} onClick={handleVisitedCountry}>{visited ? "Visited" : "Going"}</button>
+            <br />
+            <br />
+            {visited ? "I have visited this country" : "I want to visit this country"}
         </div>
     );
 };
